1d8e00d9f94c6aff1862c5b76eabe5dbb5699958
bhushan020 <96042738+bhushan020@users.noreply.github.com>
2022-01-06 22:45:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,82 @@
+/*import './App.css';
+import React from 'react'; 
+import Bhushan from './images/5.jpg' 
+
+export default function App() {
+  let list = [1,2,3,4,5,6,7,8,9];
+  
+
+  return (
+    <div className="container-fluid">
+      <Header />
+
+      <div className="row mt-1">
+        {list.map(() => (
+          <div className="col-sm-12 col-md-4 mb-1">
+            
+            <MyCard />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="row bg-dark text-white p-3 sticky-top">
+      <div className="col h3">Shoes Shopping</div>
+    
+    </div>
+      );
+}
+
+function MyCard() {
+  let cardTitle = "Shoes";
+  
+  return (
+    <div className="card">
+       
+      <div className="card-header" >{cardTitle}</div>
+      <div className="card-body" >
+      <div>
+      
+      <img src= {Bhushan} alt="" height={200} width={200}/> 
+      
+      <p>
+      Nike shoes comes with a herringbone pattern and a solid rubber, which adds to the comfort and support of users. 
+      – Nike shoes are lightweight and durable -
+      Despite provides exceptional comfort, flexibility, of Nike shoes are very light.
+      </p>
+     
+      </div>
+        <input
+          className="btn btn-sm btn-outline-secondary w-50 mt-2"
+          type="button"
+          value="Buy Now"
+        />
+        <input
+          className="btn btn-sm btn-outline-secondary w-50 mt-2"
+          type="button"
+          value="Add To Cart"
+        />
+         <input
+          className="btn btn-sm btn-outline-secondary w-50 mt-2 "
+          type="button"
+          value="Not Interested"
+        />
+         <input
+          className="btn btn-sm btn-outline-secondary w-50 mt-2 "
+          type="button"
+          value="WatchList"
+        />
+        
+      </div>
+    </div>
+  );
+}
+-----------------------------------------------------------------------------------------------------------------
+*/
+
+//----------------------------------------------Netflix----------------------------------------------------------//
+
